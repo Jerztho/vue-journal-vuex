29dcb486ca64d770f2642c74fbd8d964dac8f542
Jerztho <juan.ernesto.perez.amable@gmail.com>
2023-07-12 16:10:57 -0600

    primer commit

---
+++ src/modules/daybook/store/journal/state.js
@@ -0,0 +1,23 @@
+export default () => ({
+  isLoading: true,
+  entries: [
+    {
+      id: new Date().getTime(),
+      date: new Date().toDateString(),
+      text: 'Sint aliqua dolore adipisicing qui id duis consectetur amet nulla. Tempor consectetur proident veniam sunt proident aliqua aliquip. Et nisi officia adipisicing aliqua laborum deserunt laboris tempor eiusmod sit nisi proident aliqua eu. Adipisicing laboris Lorem anim est. Sunt labore et exercitation irure culpa voluptate fugiat. Amet mollit exercitation ex quis laborum consectetur qui eu.',
+      picture: null,
+    },
+    {
+      id: new Date().getTime() + 1000,
+      date: new Date().toDateString(),
+      text: 'Magna consectetur ex veniam enim mollit ipsum est ex eiusmod eiusmod mollit cillum officia. Laborum ipsum consectetur occaecat eiusmod anim ut dolor ullamco occaecat eiusmod labore. Commodo tempor exercitation do sit culpa magna ut duis occaecat ad minim esse. Nostrud consectetur dolore velit labore proident magna ut consectetur. Mollit eiusmod in mollit amet aliquip culpa aliquip labore enim.',
+      picture: null,
+    },
+    {
+        id: new Date().getTime() + 2000,
+        date: new Date().toDateString(),
+        text: 'Amet nisi aliqua incididunt officia exercitation laborum nulla. Reprehenderit laboris laborum ullamco eu consectetur deserunt elit et in ad dolor. Laboris laboris occaecat eiusmod dolor ut.',
+        picture: null,
+      },
+  ],
+});
